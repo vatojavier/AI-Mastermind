d6b491e9d62b2ed59356e105b5c21dc777ad81aa
s182105 <s182105@student.dtu.dk>
2019-03-23 18:28:11 +0100

    Done

--- Analytical_Test.py
+++ Analytical_Test.py
@@ -1,9 +1,21 @@
-from AIEntity import AIEntity
+from test_entity import test_entity
 import pandas as pd
 import numpy as np
 
-AI = AIEntity(5, 4)
+AI = test_entity(5, 4)
 pool = AI.pool
 k = np.zeros((625, 625))
 df = pd.DataFrame( k , columns=pool, index= pool)
-df.to_csv(path_or_buf="Address")
+df.to_csv(path_or_buf="tests_result.csv")
+
+df[(1,1,1,1)][(1,1,1,1)] = 5
+
+
+
+for i in range(10):
+    for j in range(10):
+        AI = test_entity(5, 4)
+        df[pool[i]][pool[j]] = AI.get_reduction(pool[i],pool[j])
+
+
+df.to_csv(path_or_buf="tests_result.csv")
---
+++ test_entity.py
@@ -0,0 +1,93 @@
+#AI entity that tests which guess is better
+
+import itertools
+import copy
+import numpy as np
+from random import randint
+
+
+class test_entity:
+    allColors = 0
+    nPegs = 0
+    pool = None     #Pool with all possible combinations, we don't know yet how big
+    new_guess = None    #A new guess, we don't now yet how many pegs
+    info = None     #Info given by comparing code with guess
+    reduced_pool = []
+
+    def __init__(self, allColors, nPegs):
+        self.allColors = allColors
+        self.nPegs = nPegs
+        self.pool = self.generate_pool() #generate the pool depending on number of colors
+
+    #Generates initial pool of 625 (with 5 different colors)
+    def generate_pool(self):
+        colors = []
+
+        #creates the different colors [1,2,3,4,5] or more depending on allColors
+        for i in range(self.allColors):
+            colors.append(i + 1)
+
+        #Inserting all possible permutations of colors in th pool
+        pool = [p for p in itertools.product(colors, repeat=self.nPegs)]
+        return pool
+
+    #Returns the "black and white" info by comparing guess and code params
+    def gen_info(self, guess, code):
+        info = []
+        black = 0
+        white = 0
+
+        #Converting guess and code to arrays to operate with them
+        guess = np.array(guess)
+        code = np.array(code)
+
+        #Creating empy info     REMOVES HARDCODE BUT SLOW, MAYBE ENTER PARMETER AS ARRAY INSTEAD
+        for i in range(self.nPegs):
+            info.append(None)
+
+        #info = np.array(info)
+
+        peg_compare = copy.deepcopy(guess)
+        code_compare = copy.deepcopy(code)
+
+        for i in range(self.nPegs):
+            if peg_compare[i] == code_compare[i]:
+                black += 1
+                peg_compare[i] = 0
+                code_compare[i] = -1
+
+        for i in range(self.nPegs):
+            for j in range(self.nPegs):
+                if (code_compare[i] == peg_compare[j]) and (i != j):
+                    white += 1
+                    peg_compare[j] = 0
+                    code_compare[i] = -1
+                    break
+
+        for i in range(black):
+            info[i] = 1
+
+        for i in range(black, black + white):
+            info[i] = 0
+
+        return info
+
+    #Reduces the pool and returns the reduction for the code and guess
+    def get_reduction(self, code, guess):
+        new_pool = []
+        counter = 0
+        self.new_guess = guess
+        self.info = self.gen_info(code, guess)
+
+
+        for combination in self.pool:
+            new_info = self.gen_info(combination, self.new_guess)
+            new_info = np.array(new_info)
+            if np.array_equal(new_info, self.info):
+                new_pool.append(combination)
+
+        original_size = len(self.pool)
+        new_size = len(new_pool)
+        self.pool = new_pool
+
+        return original_size - new_size
