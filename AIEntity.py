6f36ede0d42a4bf478452998d38ee3d9792b878a
s182105 <s182105@student.dtu.dk>
2019-03-30 21:37:20 +0100

    Better code and comments stuff

--- AIEntity.py
+++ AIEntity.py
@@ -1,6 +1,5 @@
 from masterMind import gen_info
 import itertools
-import copy
 import numpy as np
 from random import randint
 
@@ -8,56 +7,54 @@ from random import randint
 class AIEntity:
     allColors = 0
     nPegs = 0
-    pool = None     #Pool with all possible combinations, we don't know yet how big
-    new_guess = None    #A new guess, we don't now yet how many pegs
-    info = None     #Info given by comparing code with guess
+    pool = None  # Pool with all possible combinations, we don't know yet how big
+    new_guess = None  # A new guess, we don't now yet how many pegs
+    info = None  # Info given by comparing code with guess
     reduced_pool = []
 
+    def __init__(self, all_colors, n_pegs):
+        self.allColors = all_colors
+        self.nPegs = n_pegs
+        self.pool = self.generate_pool()  # generate the pool depending on number of colors
 
-    def __init__(self, allColors, nPegs):
-        self.allColors = allColors
-        self.nPegs = nPegs
-        self.pool = self.generate_pool() #generate the pool depending on number of colors
-
-    #Generates initial pool of 625 (with 5 different colors)
+    # Generates initial pool of 625 (with 5 different colors)
     def generate_pool(self):
         colors = []
 
-        #creates the different colors [1,2,3,4,5] or more depending on allColors
+        # creates the different colors [1,2,3,4,5] or more depending on allColors
         for i in range(self.allColors):
             colors.append(i + 1)
 
-        #Inserting all possible permutations of colors in th pool
+        # Inserting all possible permutations of colors in th pool
         pool = [p for p in itertools.product(colors, repeat=self.nPegs)]
         return pool
 
     def random_guess(self):
-        guess = self.pool[randint(0,len(self.pool) - 1)] #taking a random permutation from the pool
+        guess = self.pool[randint(0, len(self.pool) - 1)]  # taking a random permutation from the pool
         self.new_guess = guess
         return guess
 
-    #Makes a new guess to be compared with the code
+    # Makes a new guess to be compared with the code
     def smart_guess(self):
 
-        if len(self.pool) == 625: #If it's the first guees play smart guess
+        if len(self.pool) == 625:  # If it's the first guees play smart guess
             print("Smart guess played")
             guess = self.first_guess()
             self.new_guess = guess
             return guess
         else:
-            guess = self.pool[randint(0,len(self.pool) - 1)] #taking a random permutation from the pool
+            guess = self.pool[randint(0, len(self.pool) - 1)]  # taking a random permutation from the pool
             self.new_guess = guess
             return guess
 
-    #First guess that will reduce the maximum the pool
+    # First guess that will reduce the maximum the pool
     def first_guess(self):
-        smart_guess = [1,1,2,3]
+        smart_guess = [1, 1, 2, 3]
         return smart_guess
 
-    #Reduces the pool by selecting those combination that gives the same info
+    # Reduces the pool by selecting those combination that gives the same info
     def reduce_pool(self):
         new_pool = []
-        counter = 0
 
         for combination in self.pool:
             new_info = gen_info(combination, self.new_guess)
@@ -65,24 +62,21 @@ class AIEntity:
             if np.array_equal(new_info, self.info):
                 new_pool.append(combination)
 
-
         self.pool = new_pool
-
 
     def heuristic(self, guess, depth, min_h):
 
         if len(self.pool) == 1:
             return 1
 
-        #print("Depth: " + str(depth))
+        # print("Depth: " + str(depth))
         if depth == 2:
             return len(self.pool)
         depth += 1
         info_set = set()
         self.new_guess = guess
         for combination in self.pool:
-
-            info_set.add(tuple(gen_info(guess,combination)))
+            info_set.add(tuple(gen_info(guess, combination)))
 
         A = self.create_array(len(info_set))
 
@@ -99,32 +93,32 @@ class AIEntity:
         i = 0
         max_pool = []
         for info in info_set:
-            #print("info: "+ str(info))
-            #print("Lengt " + str(len(A[i].pool)))
-            #print(avg_poolsize)
+            # print("info: "+ str(info))
+            # print("Lengt " + str(len(A[i].pool)))
+            # print(avg_poolsize)
             if len(A[i].pool) >= avg_poolsize:
-                #print("Expanding pool of " + str(i))
-                H = [] #Array of length = A[i].pool
+                # print("Expanding pool of " + str(i))
+                H = []  # Array of length = A[i].pool
                 for guess in A[i].pool:
-                    #print(guess)
+                    # print(guess)
                     H.append(A[i].heuristic(guess, depth, min_h))
                 max_heuristic = min(H)
-                #print("Heuristic of " + str(info) + str(H))
+                # print("Heuristic of " + str(info) + str(H))
 
-                if (max_heuristic > min_h):
-                        #print("Max heuristic too large")
-                        return max_heuristic
+                if max_heuristic > min_h:
+                    # print("Max heuristic too large")
+                    return max_heuristic
                 max_pool.append(max_heuristic)
             i += 1
-        #print(max_pool)
+        # print(max_pool)
 
         return max(max_pool)
 
-    #Return array of AI objects
-    def create_array(self,length):
+    # Return array of AI objects
+    def create_array(self, length):
         A = []
         for i in range(length):
-            AI = AIEntity(self.allColors,self.nPegs)
+            AI = AIEntity(self.allColors, self.nPegs)
             AI.pool = self.pool
             AI.new_guess = self.new_guess
             A.append(AI)
--- masterMind.py
+++ masterMind.py
@@ -4,128 +4,130 @@ import argparse
 import copy
 import random
 
-#Prepares parser for initial options (for testing stuff)
+
+# Prepares parser for initial options (for testing stuff)
 def prep_parser():
     parser = argparse.ArgumentParser(description="Master Mind Game with AI I guess")
 
-    parser.add_argument("-c","--code",dest="code_gen",
-        help="The way the goal code is generated (random or manual), default = random",default="random")
+    parser.add_argument("-c", "--code", dest="code_gen",
+                        help="The way the goal code is generated (random or manual), default = random",
+                        default="random")
 
-    parser.add_argument("-n","--ncolors",dest="all_colors",
-        help="Number of total available colors",default="5")
+    parser.add_argument("-n", "--ncolors", dest="all_colors",
+                        help="Number of total available colors", default="5")
 
-    parser.add_argument("-p","--nPegs",dest="nPegs",
-        help="Number of pegs (holes)",default="4")
+    parser.add_argument("-p", "--nPegs", dest="nPegs",
+                        help="Number of pegs (holes)", default="4")
 
     options = parser.parse_args()
 
     return options
 
-#Makes the human choose a color
-def human_choose_color(all_colors,nPegs):
 
-    code = np.zeros((nPegs,), dtype=int)
+# Makes the human choose a color
+def human_choose_color(total_colors, n_pegs):
+    goal_code = np.zeros((n_pegs,), dtype=int)
 
-    print("Available colors: " + str(all_colors))
+    print("Available colors: " + str(total_colors))
     print("Choose goal color code: ")
 
-    i = 0
-    while i < nPegs:
-        color = int(input("Color " + str(i+1) + ": "))
-        if (color > 0 and color <= all_colors):
-            code[i] = color
-            i = i + 1
+    inserted = 0
+    while inserted < n_pegs:
+        color = int(input("Color " + str(inserted + 1) + ": "))
+        if 0 < color <= total_colors:
+            goal_code[inserted] = color
+            inserted = inserted + 1
         else:
-            print("Enter a valid color, available colors: " + str(all_colors))
-    print(code)
-    return code
+            print("Enter a valid color, available colors: " + str(total_colors))
+    print(goal_code)
+    return goal_code
 
 
 ###         functions of the board      ###
 
-#Just creates the [Black, Black, Black, Black] info
-def generate_goal_info(nPegs):
-    goal_info = []
-    for i in range(nPegs):
-        goal_info.append(1);
+# Just creates the [Black, Black, Black, Black] info
+def generate_goal_info(n_pegs):
+    goal = []
+    for peg in range(n_pegs):
+        goal.append(1)
 
-    return goal_info
+    return goal
 
-#Returns the "black and white" info by comparing guess and code params
-def gen_info(guess, code):
+
+# Returns the "black and white" info by comparing guess and code_comp params
+def gen_info(new_guess, code_comp):
     info = []
     black = 0
     white = 0
-    nPegs = len(guess)
+    peg_number = len(new_guess)
 
-    #Converting guess and code to arrays to operate with them
-    guess = np.array(guess)
-    code = np.array(code)
+    # Converting guess and code to arrays to operate with them
+    new_guess = np.array(new_guess)
+    code_comp = np.array(code_comp)
 
-    #Creating empy info     REMOVES HARDCODE BUT SLOW, MAYBE ENTER PARMETER AS ARRAY INSTEAD
-    for i in range(nPegs):
+    # Creating empty info
+    for peg in range(peg_number):
         info.append(None)
 
-    #info = np.array(info)
+    # info = np.array(info)
 
-    peg_compare = copy.deepcopy(guess)
-    code_compare = copy.deepcopy(code)
+    peg_compare = copy.deepcopy(new_guess)
+    code_compare = copy.deepcopy(code_comp)
 
-    for i in range(nPegs):
-        if peg_compare[i] == code_compare[i]:
+    for peg in range(peg_number):
+        if peg_compare[peg] == code_compare[peg]:
             black += 1
-            peg_compare[i] = 0
-            code_compare[i] = -1
+            peg_compare[peg] = 0
+            code_compare[peg] = -1
 
-    for i in range(nPegs):
-        for j in range(nPegs):
-            if (code_compare[i] == peg_compare[j]) and (i != j):
+    for peg in range(peg_number):
+        for j in range(peg_number):
+            if (code_compare[peg] == peg_compare[j]) and (peg != j):
                 white += 1
                 peg_compare[j] = 0
-                code_compare[i] = -1
+                code_compare[peg] = -1
                 break
 
-    for i in range(black):
-        info[i] = 1
+    for peg in range(black):
+        info[peg] = 1
 
-    for i in range(black, black + white):
-        info[i] = 0
+    for peg in range(black, black + white):
+        info[peg] = 0
 
     return info
 
-#Generates goal code randomly
-def generate_code(allColors,nPegs):
-    code = []
+
+# Generates goal code randomly
+def generate_code(total_colors, peg_number):
+    goal_code = []
     colors = []
 
-    for i in range (1,allColors + 1):
+    for i in range(1, total_colors + 1):
         colors.append(i)
 
-    for p in range(nPegs):
-        code.append(random.choice(colors))
+    for p in range(peg_number):
+        goal_code.append(random.choice(colors))
 
-    #code = np.random.randint(1,high=allColors + 1, size=nPegs)
-    return code
+    # code = np.random.randint(1,high=allColors + 1, size=nPegs)
+    return goal_code
+
 
 ##      main program    ##
-if __name__== "__main__":
+if __name__ == "__main__":
 
     options = prep_parser()
     nPegs = int(options.nPegs)
     goal_info = generate_goal_info(nPegs)
     all_colors = int(options.all_colors)
 
-
     if options.code_gen == "random":
-        code = generate_code(all_colors, nPegs) #code will be generate randomly
+        code = generate_code(all_colors, nPegs)  # code will be generate randomly
     else:
-        code = human_choose_color(all_colors, nPegs) #code will be selected by human
-
+        code = human_choose_color(all_colors, nPegs)  # code will be selected by human
 
     print(goal_info)
 
-
-    #Creating AI entity
+    # Creating AI entity
     AI = AIEntity.AIEntity(all_colors, nPegs)
 
     print(str(code) + "<---Code")
