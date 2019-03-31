a5b6df048051898fda1262eb44426108f55a47e2
s182105 <s182105@student.dtu.dk>
2019-03-31 13:05:57 +0200

    Added test and print debugs

--- AIEntity.py
+++ AIEntity.py
@@ -30,31 +30,6 @@ class AIEntity:
         pool = [p for p in itertools.product(colors, repeat=self.nPegs)]
         return pool
 
-    # Generates an actual guess depending in the step and computing the heuristic search
-    def generate_guess(self):
-
-        if self.step == 0:  # If it's the first guess...
-            self.new_guess = self.first_guess()
-
-        else:   # Otherwise search the tree to generate heuristics
-            h = np.zeros(len(self.pool))
-            j = 0
-            min_h = len(self.pool)
-
-            for guess in self.pool:
-                h[j] = self.heuristic(guess, 0, min_h)
-
-                if h[j] < min_h:
-                    min_h = h[j]
-                j = +1
-
-            h_min_index = np.argmin(h)
-            best_guess = self.pool[h_min_index]
-            self.new_guess = best_guess
-
-        self.step += 1
-        return self.new_guess
-
     def random_guess(self):
         guess = self.pool[randint(0, len(self.pool) - 1)]  # taking a random permutation from the pool
         self.new_guess = guess
@@ -64,7 +39,7 @@ class AIEntity:
     @staticmethod
     def first_guess():
 
-        smart_guess = [1, 2]
+        smart_guess = [1, 1]
         return smart_guess
 
     # Reduces the pool by selecting those combination that gives the same info
@@ -79,45 +54,83 @@ class AIEntity:
 
         self.pool = new_pool
 
+    # Generates an actual guess choosing the MINIMUM heuristic
+    def generate_guess(self):
+
+        if self.step == 70:  # If it's the first guess...
+            self.new_guess = self.first_guess()
+
+        else:  # Otherwise search the tree to generate heuristics
+            h = np.zeros(len(self.pool), dtype=int)
+            j = 0
+            min_h = len(self.pool)
+
+            for guess in self.pool:
+                h[j] = self.heuristic(guess, 0, min_h)
+                print("Heuristic of guess" + str(guess) + " is " + str(h[j]))
+                if h[j] < min_h:
+                    min_h = h[j]
+                j = +1
+
+            h_min_index = np.argmin(h)
+            print("The min index is: " + str(h_min_index))
+            best_guess = self.pool[h_min_index]
+            self.new_guess = best_guess
+            print(h)
+            print("So the new guess is: " + str(self.new_guess))
+        self.step += 1
+        return self.new_guess
+
+    # Generates heuristic by returning the MAXIMUM pool size of the pools at selected depth
     def heuristic(self, guess, depth, min_h):
+        self.c_print("For guess " + str(guess) + ": ", depth)
+        self.c_print("Depth: " + str(depth),depth)
 
         if len(self.pool) == 1:
+            self.c_print("Min pool size of 1 reached", depth)
             return 1
 
         # print("Depth: " + str(depth))
         if depth == 2:
+            self.c_print("Max depth reached, returning pool size of " + str(len(self.pool)), depth)
             return len(self.pool)
+
         depth += 1
         info_set = set()
         self.new_guess = guess
-        for combination in self.pool:
+        for combination in self.pool:  # Generating all infos for every combination in the pool:
             info_set.add(tuple(gen_info(guess, combination)))
 
         entity_array = self.create_array(len(info_set))
 
         i = 0
         pools_sum = 0
+        self.c_print("Possible infos: ", depth)
         for info in info_set:
             entity_array[i].info = list(info)
             entity_array[i].reduce_pool()
+            self.c_print(str(info) + " with pool size of " + str(len(entity_array[i].pool)), depth)
             pools_sum += len(entity_array[i].pool)
             i += 1
 
         avg_poolsize = pools_sum / i
+        self.c_print("Average pool size of depth " + str(depth) + ": " + str(avg_poolsize), depth)
 
         max_pool = []
         for i in range(len(info_set)):
+            self.c_print("Pool of this info: " + str(entity_array[i].pool), depth)
             if len(entity_array[i].pool) >= avg_poolsize:
                 heuristic = []  # Array of length = A[i].pool
                 for guess in entity_array[i].pool:
                     heuristic.append(entity_array[i].heuristic(guess, depth, min_h))
-                max_heuristic = min(heuristic)
+                min_heuristic = min(heuristic)
 
-                if max_heuristic > min_h:
-                    return max_heuristic
-                max_pool.append(max_heuristic)
+                if min_heuristic > min_h:
+                    return min_heuristic
+                max_pool.append(min_heuristic)  # Adding to max_pool the minimum heuristic
 
-        return max(max_pool)
+        self.c_print("Returning " + str(max(max_pool)), depth)
+        return max(max_pool)  # Returning the maximum of the minimum heuristic haha
 
     # Return array of AI objects
     def create_array(self, length):
@@ -128,3 +141,11 @@ class AIEntity:
             AI.new_guess = self.new_guess
             array.append(AI)
         return array
+
+    def c_print(self, arg, depth):
+
+        for i in range(depth):
+            print("\t", end=" ")
+
+        print(arg)
+
---
+++ heuristic_test.py
@@ -0,0 +1,132 @@
+import AIEntity
+import numpy as np
+import argparse
+import copy
+import random
+
+
+# Prepares parser for initial options (for testing stuff)
+def prep_parser():
+    parser = argparse.ArgumentParser(description="Master Mind Game with AI I guess")
+
+    parser.add_argument("-c", "--code", dest="code_gen",
+                        help="The way the goal code is generated (random or manual), default = random",
+                        default="random")
+
+    parser.add_argument("-n", "--ncolors", dest="all_colors",
+                        help="Number of total available colors", default="5")
+
+    parser.add_argument("-p", "--nPegs", dest="nPegs",
+                        help="Number of pegs (holes)", default="4")
+
+    parser_options = parser.parse_args()
+
+    return parser_options
+
+
+# Makes the human choose a color
+def human_choose_color(total_colors, peg_number):
+    new_code = np.zeros((peg_number,), dtype=int)
+
+    print("Available colors: " + str(total_colors))
+    print("Choose goal color code: ")
+
+    inserted = 0
+    while inserted < peg_number:
+        color = int(input("Color " + str(inserted + 1) + ": "))
+        if 0 < color <= total_colors:
+            new_code[inserted] = color
+            inserted = inserted + 1
+        else:
+            print("Enter a valid color, available colors: " + str(total_colors))
+    print(new_code)
+    return new_code
+
+
+#        functions of the board      #
+
+# Just creates the [Black, Black, Black, Black] info
+def generate_goal_info(n_pegs):
+    goal = []
+    for peg in range(n_pegs):
+        goal.append(1)
+
+    return goal
+
+
+# Returns the "black and white" info by comparing guess and code params
+def gen_info(guess, code_comp):
+    info = []
+    black = 0
+    white = 0
+    peg_number = len(guess)
+
+    # Converting guess and code to arrays to operate with them
+    guess = np.array(guess)
+    code_comp = np.array(code_comp)
+
+    # Creating empty info
+    for peg in range(peg_number):
+        info.append(None)
+
+    # info = np.array(info)
+
+    peg_compare = copy.deepcopy(guess)
+    code_compare = copy.deepcopy(code_comp)
+
+    for peg in range(peg_number):
+        if peg_compare[peg] == code_compare[peg]:
+            black += 1
+            peg_compare[peg] = 0
+            code_compare[peg] = -1
+
+    for peg in range(peg_number):
+        for j in range(peg_number):
+            if (code_compare[peg] == peg_compare[j]) and (peg != j):
+                white += 1
+                peg_compare[j] = 0
+                code_compare[peg] = -1
+                break
+
+    for peg in range(black):
+        info[peg] = 1
+
+    for peg in range(black, black + white):
+        info[peg] = 0
+
+    return info
+
+
+# Generates goal code randomly
+def generate_code(total_colors, peg_number):
+    goal_code = []
+    colors = []
+
+    for new_color in range(1, total_colors + 1):
+        colors.append(new_color)
+
+    for p in range(peg_number):
+        goal_code.append(random.choice(colors))
+
+    # code = np.random.randint(1,high=allColors + 1, size=nPegs)
+    return goal_code
+
+
+#      main program    #
+if __name__ == "__main__":
+
+    options = prep_parser()
+    nPegs = int(options.nPegs)
+    goal_info = generate_goal_info(nPegs)
+    all_colors = int(options.all_colors)
+
+    if options.code_gen == "random":
+        code = generate_code(all_colors, nPegs)  # code will be generate randomly
+    else:
+        code = human_choose_color(all_colors, nPegs)  # code will be selected by human
+
+    AI = AIEntity.AIEntity(all_colors, nPegs)
+
+    #h = AI.heuristic((1, 3), 0, 10000)
+    #print(h)
+    AI.generate_guess()
